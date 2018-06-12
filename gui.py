3b3d4fd5dcbcb02ab3a83fc29b6491224d29fcd9
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2018-06-12 09:39:12 -0700

    gui.py

--- gui.py
+++ gui.py
@@ -3,11 +3,17 @@ from tkinter import *
 #Create empty window
 window = Tk()
 
-b1 = Button(window, text="Press Me")
+def km_to_miles():
+    print(e1_value.get())
+    miles = float(e1_value.get()) * 1.60934
+    t1.insert(END, miles)
+
+b1 = Button(window, text="Convert to Miles", command=km_to_miles)
 b1.grid(row=0, column=0)
 #b1.pack()
 
-e1 = Entry(window)
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
 t1 = Text(window, height=1, width=20)

4dbe33cd25943ad8c8a846bf7858d081f69cbbfe
Andrei Kniazev <akniazev@gmail.com>
2018-06-18 19:04:25 +0000

    Add matplotlib functions to the app

--- graph_gui.py
+++ graph_gui.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+#from matplotlib.figure import Figure
+import matplotlib.pyplot as plt 
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -18,7 +23,7 @@ class SeaofBTCapp(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne, PageTwo, PageThree):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -42,6 +47,10 @@ class StartPage(tk.Frame):
         button2 = ttk.Button(self, text="Visit Page 2", 
                 command = lambda: controller.show_frame(PageTwo))
         button2.pack()
+
+        button3 = ttk.Button(self, text="Graph Page", 
+                command = lambda: controller.show_frame(PageThree))
+        button3.pack()
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
@@ -71,6 +80,43 @@ class PageTwo(tk.Frame):
                 command = lambda: controller.show_frame(PageOne))
         button2.pack()
 
+class PageThree(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page", font = LARGE_FONT)
+        label.pack(pady = 10, padx = 10)
+
+        button1 = ttk.Button(self, text="Back to Home", 
+                command = lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+
+        f = plt.figure()
+        a = f.add_subplot(111)
+        a = f.add_subplot(111)  # one by one and this is one
+        a.plot([1,2,3,4,5,6,7,8], [5,6,1,3,8,9,3,5])
+        print("created plot")
+"""
+        canvas = FigureCanvasTkAgg(f, self)
+        print("created canvas")
+        canvas.show()
+        print("showing canvas")
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        f = Figure(figsize = (5,5), dpi = 100)
+        a = f.add_subplot(111)  # one by one and this is one
+        a.plot([1,2,3,4,5,6,7,8], [5,6,1,3,8,9,3,5])
+#        plt.show() # In normal operation
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+ """       
 
 
 
